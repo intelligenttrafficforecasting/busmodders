9618dd54540b0f6b9179090b09e0cef992930540
jeppe johan waarkjær olsen <jeppe742@gmail.com>
2018-11-22 13:55:49 +0100

    merge

--- model/CNN.py
+++ model/CNN.py
@@ -1,88 +1,89 @@
 import torch
-from torch.autograd import Variable
-from torch.nn.parameter import Parameter
-from model.BusmodderNet import BusmodderNet
+
+#from torch.autograd import Variable
+#from torch.nn.parameter import Parameter
+from BaseNetwork import BaseNetwork
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.nn.init as init
 
-from torch.nn import Linear, Conv1d, BatchNorm1d, MaxPool1d, Dropout
-from torch.nn.functional import relu, elu, relu6, sigmoid, tanh, softmax
+from torch.nn.modules import Module
 
+#from torch.nn import Linear, Conv1d, BatchNorm1d, MaxPool1d, Dropout
+#from torch.nn.functional import relu, elu, relu6, sigmoid, tanh, softmax
+from torch.nn import Module, Linear, ReLU, Sequential
+from BaseNetwork import BaseNetwork
+from torch.optim import Adam
 import sys
 sys.path.append("../DCRNN")
 from lib.utils import calculate_normalized_laplacian
 from misc.data_loader import load_network, adjacency_matrix
 
-class CNN(BusmodderNet):
 
-    def __init__(self):
-        super(CNN, self).__init__()
+class ConvolutionLayer(Module):
+    def __init__(self,laplacian,n_signals,n_out):
+        super(ConvolutionLayer,self).__init__()
+        self.n_nodes = laplacian.shape[0]
+        self.laplacian = laplacian
+        self.l1  = Linear(n_signals * self.n_nodes, n_out * self.n_nodes,bias = False)
+    
+    def forward(self,x):
+        y = self.laplacian.matmul(x)
+        shape = y.shape
+        y = y.reshape(shape[0],shape[1]*shape[2])
+        y = self.l1(y)
+        y = y.reshape(shape)
+        return y
+
+
+class CNN(BaseNetwork):
+    def __init__(self, previous_timesteps, num_hidden):
+    #def __init__(self, num_hidden):
+        super(CNN,self).__init__()
         self.road_network = load_network()
         self.adj_mat = adjacency_matrix(self.road_network)
-        self.laplacian = calculate_normalized_laplacian(self.adj_mat)
-        #out_dim = (input_dim - filter_dim + 2padding) / stride + 1
-        channels = 1
-        kernel_size = 10
-        stride = 0
-        padding = 0
-        #TODO: should be read instead of hardcoded
-        feature_size = 194
-        self.conv_1 = Conv1d(in_channels=1,
-                            out_channels=channels,
-                            kernel_size=kernel_size,
-                            stride=stride,
-                            padding=padding)
-        #
-        #self.conv_out_height1 = compute_conv_dim(height)
-        #self.conv_out_width1 = compute_conv_dim(width)
-        #self.conv1_in_features1 = channels * height * width
+        #first order approx
+        self.n_nodes = self.adj_mat.shape[0]
+        sparse_mat = calculate_normalized_laplacian(self.adj_mat + torch.eye(self.n_nodes))
+        self.laplacian = torch.tensor(sparse_mat.todense()).float()
+        #self.convl1 = ConvolutionLayer(self.laplacian,(previous_timesteps+1),1)
+        self.relu = ReLU()
+        self.CNN = Sequential(
+            ConvolutionLayer(self.laplacian,previous_timesteps+1, previous_timesteps+1),
+            ReLU(),
+            ConvolutionLayer(self.laplacian, previous_timesteps+1,previous_timesteps+1),
+            ReLU(),
+            Linear((previous_timesteps+1), 1),
+        )
         
-        max_pool_1_size = 2
-        self.max_pool_1 = MaxPool1d(kernel_size=max_pool_1_size, stride=2)
-        #self.max_pool_out_height1 = compute_max_pool_dim(self.conv_out_height1)
-        #self.max_pool_out_width1 = compute_max_pool_dim(self.conv_out_width1)
-        
-        #self.conv_2 = Conv2d(in_channels=num_filters_conv1,
-        #                    out_channels=num_filters_conv1,
-        #                    kernel_size=kernel_size_conv1,
-        #                    stride=stride_conv1,
-        #                    padding=padding_conv1)
-        
-        
-        #self.conv_out_height2 = compute_conv_dim(self.max_pool_out_height1)
-        #self.conv_out_width2 = compute_conv_dim(self.max_pool_out_width1)
-        #self.conv1_in_features2 = channels * self.conv_out_height1 * self.conv_out_width1
-        
-        #self.max_pool_2 = MaxPool2d(kernel_size=max_pool_1_size, stride=2)
-        #self.max_pool_out_height2 = compute_max_pool_dim(self.conv_out_height2)
-        #self.max_pool_out_width2 = compute_max_pool_dim(self.conv_out_width2)
-        
-        # add dropout to network
-        #self.dropout = Dropout2d(p=0.5)
-        self.l1_in_features = channels * feature_size
-        #self.l1_in_features = channels * height * width
-        
-        #self.l_1 = Linear(in_features=self.l1_in_features, 
-        #                  out_features=num_l1,
-        #                  bias=True)
-        self.l_out = Linear(in_features=self.l1_in_features, 
-                            out_features=feature_size,
-                            bias=False)
     
-    def forward(self, x): # x.size() = [batch, channel, height, width]
-        x = relu(self.conv_1(x))
-        #x = self.dropout(relu(self.conv_1(x)))
-        #x = self.max_pool_1(x)
-        #x = self.dropout(relu(self.conv_2(x)))
-        #x = self.max_pool_2(x)
-        # torch.Tensor.view: http://pytorch.org/docs/master/tensors.html?highlight=view#torch.Tensor.view
-        #   Returns a new tensor with the same data as the self tensor,
-        #   but of a different size.
-        # the size -1 is inferred from other dimensions 
+    
+    def forward(self,x):
+        """
+        x : [batch_size, prev_timesteps, num_roads]
         
+        """
+        #Transpose input, such that the previous time steps are the last dimension
+        x = x.transpose(2,1)
         
-        #x = self.dropout(relu(self.l_1(x)))
-        #x = relu(self.l_1(x))
-        return relu(self.l_out(x), dim=1)
+        predictions = []
+        for _ in range(self.max_timestep):
+            #Run the input through the network
+            y = self.CNN(x).squeeze()
+            #y = self.convl1(x)
+            #y = self.relu(y)
+            #Append the prediction to the list of predictions
+            predictions.append(y[:,:self.num_roads])
+
+            #remove oldest timestep
+            x = x[:,:,1:]
+            #unsqueeze output so its size is [batch_size, num_roads, timesteps]
+            y = y.unsqueeze(2)
+
+            #append the new prediction to the input
+            x = torch.cat((x,y),dim=2)
+
+
+        return torch.stack(predictions,1)
+
 
